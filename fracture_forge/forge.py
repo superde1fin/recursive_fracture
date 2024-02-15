c5d832a9b23384cdf4b058cf44a18f545126fd3f
Vasilii Maksimov <kopolokrbrgr@gmail.com>
2024-02-15 11:13:46 -0500

    End of recursion test

--- fracture_forge/forge.py
+++ fracture_forge/forge.py
@@ -1,6 +1,8 @@
 from lammps import PyLammps
 import re, glob, copy, os
 from classes.Storage import Data, SystemParams
+import numpy as np
+import time, random
 
 
 def system_parameters_initialization(lmp, units = "metal"):
@@ -32,13 +34,15 @@ def vizualization(lmp, thermo_step = 0.1, dump_step = 0.1, name = ""): #ns
 
 
 def copy_lmp(lmp):
+    os.system("touch log.lammps && pwd > log.lammps")
+    """
     new_lmp = PyLammps(verbose = False)
     system_parameters_initialization(new_lmp, units = "real")
-    lmp.timestep(1)
+    new_lmp.timestep(1)
     filename = glob.glob("glass_*.structure")[-1]
     name_handle = re.search(r"(?<=glass_).+(?=\.structure)", filename).group()
-    lmp.include(f"pot_{name_handle}.FF")
-    vizualization(lmp, name = name_handle)
+    new_lmp.include(f"pot_{name_handle}.FF")
+    vizualization(new_lmp, name = name_handle)
 
     new_lmp.region("my_simbox block", lmp.system.xlo, lmp.system.xhi, lmp.system.ylo, lmp.system.yhi, lmp.system.zlo, lmp.system.zhi)
     new_lmp.create_box(lmp.system.ntypes, "my_simbox")
@@ -46,33 +50,45 @@ def copy_lmp(lmp):
     for atom in lmp.atoms:
         new_lmp.create_atoms(atom.type, "single", " ".join(map(str, atom.position)))
 
-    os.system()
     return new_lmp
+    """
+    return lmp
 
 
 def quazi_static(lmp, dr_frac = 0.01, dtheta = 1):
-    start_coords = (lmp.eval("lx")/2 + lmp.system.xhi, 0)
-    new_lmp = QSR(lmp = lmp, coords = start_coords, dr = dr_frac*max(lmp.eval("lx"), lmp.eval("ly"), lmp.eval("lz")), dtheta = dtheta, norm = (1, 0))
+    print("start")
+    start_coords = (lmp.eval("lx")/2 + lmp.system.xlo, lmp.system.ylo)
+    starting_dir = "initial"
+    if not os.path.isdir(starting_dir):
+        os.system(f"mkdir {starting_dir}")
+    else:
+        os.system(f"rm -r {starting_dir}/*")
+    os.chdir("initial")
+#    new_lmp = QSR(lmp = lmp, coords = start_coords, dr = dr_frac*max(lmp.eval("lx"), lmp.eval("ly"), lmp.eval("lz")), dtheta = dtheta, norm = (1, 0), theta = None, first = True)
+    new_lmp = QSR(lmp = float("infinity"), coords = start_coords, dr = dr_frac*max(lmp.eval("lx"), lmp.eval("ly"), lmp.eval("lz")), dtheta = dtheta, norm = (1, 0), theta = None, first = True)
 
     return (lmp.eval("pe") - new_lmp.eval("pe"))/new_lmp.variables["surface_area"].value
 
 
+def QSR(lmp, coords, dr, dtheta, norm, theta, first = False):
+    if theta:
+        lmp = copy_lmp(lmp)
+#        lmp.variable(f"surface_area equals {dr*lmp.eval('lz')}")
+    print(coords)
+    os.system("pwd")
 
-def QSR(lmp, coords, dr, dtheta, norm, theta):
-    lmp = copy_lmp(lmp)
-    return 
+    if (coords[0] > 43 or coords[0] < 3.4 or coords[1] > 43 or coords[1] < 3.4) and not first:
+        print("hit border")
+        return random.randint(20, 100)
 
-    if coords[0] > lmp.eval("lx") + lmp.eval("xlo") or coords[0] < lmp.eval("xlo") or coords[1] > lmp.eval("ly") + lmp.eval("ylo") or coords[1] < lmp.eval("ylo"):
+    """
+    if coords[0] > lmp.system.xhi or coords[0] < lmp.system.xlo or coords[1] > system.yhi or coords[1] < lmp.system.ylo:
         return lmp
-
-    """
-    if "surface_area" not in lmp.variables.keys():
-        lmp.variable("surface_area equals 0.0")
-    else:
-        lmp.variable("surface_area equals ${surface_area}")
     """
 
+#    lmp.variable(f"surface_area equals {lmp.eval('surface_area') + dr*lmp.eval('lz')}")
 
+    """
     lmp.fix(f"probe all npt temp 300 300 {lmp.eval('dt')*1000} iso 1 1 {lmp.eval('dt')*1000}")
     lmp.region(f"probe_region {coords[0]} {coords[1]} 0.0 {norm[0]} {norm[1]} 0")
     #Turn off interactions manually
@@ -80,26 +96,32 @@ def QSR(lmp, coords, dr, dtheta, norm, theta):
     probe_time = convert_timestep(lmp, 0.05)
     lmp.run(probe_time)
     lmp.unfix("probe")
+    """
 
 
     lowest_pot = float("infinity")
     res_lmp = lmp
-    for theta in np.linspace(0, np.pi, int(180/dtheta)):
-        os.system(f"mkdir {theta} && cd {theta}")
-        tmp_lmp =  QSR(lmp, coords = (coords[0] + dr*np.cos(theta), coords[1] + dr*np.sin(theta)), dr = dr, dtheta = dtheta, norm = (np.cos(theta - np.pi/2), np.sin(theta - np.pi/2)), theta)
-        os.system("cd ..")
-        new_pe = tmp_lmp.eval("pe")
+    for theta in np.linspace(0, np.pi, int(180/dtheta) + 1):
+        print(str(theta))
+        os.system(f"mkdir {theta}")
+        os.chdir(f"{theta}")
+        tmp_lmp =  QSR(lmp, coords = (coords[0] + dr*np.cos(theta), coords[1] + dr*np.sin(theta)), dr = dr, dtheta = dtheta, norm = (np.cos(theta - np.pi/2), np.sin(theta - np.pi/2)), theta = theta)
+        os.chdir("..")
+#        new_pe = tmp_lmp.eval("pe")
+        new_pe = tmp_lmp
+        print(f"lowest pe: {new_pe}")
         if new_pe < lowest_pot:
             os.system(f"cp {theta}/log.lammps .")
             selected_angle = theta
             lowest_pot = new_pe
 #            res_lmp.finalize()
-            res_lmp.close()
+#            res_lmp.close()
             res_lmp = tmp_lmp
         else:
+            pass
 #            tmp_lmp.finalize()
-            tmp_lmp.close()
-        os.system(f"rm {theta}")
+#            tmp_lmp.close()
+        os.system(f"rm -r {theta}")
 
     """
     for log_name in glob.glob(f"log_{lvl_ctr + 1}_*.lammps"):
@@ -140,7 +162,8 @@ def main():
     lmp.unfix("initial_relax")
     """
 
-    quazi_static(lmp)
+#    quazi_static(lmp)
+    quazi_static(lmp, 0.5, 90)
 
     return lmp
 
